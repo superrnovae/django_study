45672058e62b4af8f7c5a03d8399027c7938de11
parphelion <vladislavp@pm.me>
2022-06-16 23:56:22 +0200

    Set up the database connection

--- study/settings.py
+++ study/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'study.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'vladislav',
+        'PASSWORD': 'Z3S8cysgftM2REMQJsQ2',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'TIME_ZONE': 'Europe/Paris'
     }
 }
 
