c672a595585589ad904c3dc311d90fcb114ad095
parphelion <vladislavp@pm.me>
2022-06-17 00:41:31 +0200

    Create first models and migrate them

--- example/models.py
+++ example/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=32)
+    user_age = models.IntegerField(default=0)
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date de publication')
+
+
+
